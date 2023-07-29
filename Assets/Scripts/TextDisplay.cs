3248ad08d9d4be373b9450abf2d31b76cc6d326f
DESKTOP-8Q3LJDA\Maxen <flick.magic.0o@icloud.com>
2023-07-30 00:12:10 +1000

    Refined the game to add more gameplay with the hazard, implementing a bridge over a lava pit and parkour obby.

--- Assets/Scripts/TextDisplay.cs
+++ Assets/Scripts/TextDisplay.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class TextDisplay : MonoBehaviour
 {
-    public GameObject chicken;
-    public GameObject player;
-
     // Start is called before the first frame update
     void Start()
     {
-        player = (Instantiate(chicken, new Vector3(0, 0, 0), Quaternion.identity));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
